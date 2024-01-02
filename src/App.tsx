4408e6a8ceb0877398896a19861ed5aa65f9095f
asrebel <asrebel777@gmail.com>
2024-01-02 23:54:55 +0500

    to-do-lesson-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TaskType, TodoList} from "./TodoList";
 
 function App() {
 
-    const truck1 = 'What to learn'
-    const truck2 = 'What to buy'
+    const tasks: Array<TaskType> = [
+        {id: 1, title: 'HTML', isDone: false},
+        {id: 2, title: 'CSS', isDone: false},
+        {id: 3, title: 'JS/TS', isDone: true},
+        {id: 4, title: 'React', isDone: true},
 
-    const tasks1 = [
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'ReactJS', isDone: false},
     ]
 
-    const tasks2 = [
-        {id: 1, title: 'Hello World', isDone: true},
-        {id: 2, title: 'I am happy', isDone: false},
-        {id: 3, title: 'Yo', isDone: false},
-    ]
-
+    const todoListTitle = 'What to learn'
 
     return (
         <div className="App">
-            <Todolist truck = {truck1} tasks = {tasks1}/>
-            <Todolist truck = {truck2} tasks = {tasks2}/>
+            <TodoList title={todoListTitle}
+                      tasks={tasks}
+            />
         </div>
     );
 }
